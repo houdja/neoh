c77b83e007e0371bcda291764926090d23d80952
Houdja <houdja-ahamadi976@hotmail.com>
2022-08-21 18:03:45 +0400

    script srollToTop

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ let bar1 = document.querySelector('.bar1');
 let bar2 = document.querySelector('.bar2');
 let bar3 = document.querySelector('.bar3');
 let menu = document.querySelector('.menu');
+let upBtn = document.querySelector('.up-btn');
 
 hamburger.addEventListener('click', function(){
     bar1.classList.toggle('active');
     bar2.classList.toggle('active');
     bar3.classList.toggle('active');
     menu.classList.toggle('active');
+})
+
+upBtn.addEventListener('click', function(){
+    window.scrollTo(0, 0);
+})
+
+window.addEventListener('scroll', (e) => {  
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        upBtn.style.display = "flex";
+      } else {
+        upBtn.style.display = "none";
+      } 
 })
